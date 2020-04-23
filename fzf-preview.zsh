971202cacc9a6d74c86fabdd1f5d761c87be197f
Milly <milly.ca@gmail.com>
2020-04-24 02:07:26 +0900

    Do not repeat variable name to define default value.

--- fzf-preview.zsh
+++ fzf-preview.zsh
@@ -1,8 +1,8 @@
 FZF_PREVIEW_BIN_PATH="${0:a:h}/bin"
 FZF_PREVIEW_CONFIG_DIR="${0:a:h}/config"
-FZF_PREVIEW_DISABLE_DEFAULT_SETTING=${FZF_PREVIEW_DISABLE_DEFAULT_SETTING:-0}
-FZF_PREVIEW_USER_CONFIG_DIR=${FZF_PREVIEW_USER_CONFIG_DIR:-"${XDG_CONFIG_HOME}/fzf-preview.zsh"}
-FZF_PREVIEW_DEFAULT_BIND=${FZF_PREVIEW_DEFAULT_BIND:-'ctrl-d:preview-page-down,ctrl-u:preview-page-up,?:toggle-preview'}
+: ${FZF_PREVIEW_DISABLE_DEFAULT_SETTING:=0}
+: ${FZF_PREVIEW_USER_CONFIG_DIR:="${XDG_CONFIG_HOME}/fzf-preview.zsh"}
+: ${FZF_PREVIEW_DEFAULT_BIND:='ctrl-d:preview-page-down,ctrl-u:preview-page-up,?:toggle-preview'}
 
 fpath+=${0:a:h}/src/completion/util
 for f in ${0:h}/src/completion/util/*(N-.); do
